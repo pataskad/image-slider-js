40f32639e99ac16d90faf9562b226d9daf58417a
Dallas Pataska <pataskad@gmail.com>
2021-12-08 14:28:11 -0600

    basic project outline

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+	mode: "development",
+	entry: "./src/index.js",
+	devtool: "inline-source-map",
+	devServer: {
+		static: "./dist",
+		compress: true,
+		port: 5023,
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: "Mobile Hamburger UI",
+			template: "./src/index.html",
+		}),
+	],
+	output: {
+		filename: "main.js",
+		path: path.resolve(__dirname, "dist"),
+		clean: true,
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: ["style-loader", "css-loader"],
+			},
+			{
+				test: /\.(png|jpg|gif|svg|jpeg)$/i,
+				type: "asset/resource",
+			},
+		],
+	},
+};
